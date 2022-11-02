7d8cdbe843f87405041657891245b428442f232e
Michael H. Oshita <ijinpublic+github@gmail.com>
2022-11-02 11:16:26 +0900

    add header response

--- server.py
+++ server.py
@@ -7,6 +7,9 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.log_message("%s %s\n%s", self.command, self.path, self.headers)
 
     def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain; charset=utf-8')
+        self.end_headers()
         self.print_info()
         self.wfile.write(("path: " + self.path + "\n").encode('utf-8'))
 
